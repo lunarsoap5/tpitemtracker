8bddd15b12de42d8b0a341661a641636fc67ef71
lunarsoap5 <justindakotacarter@gmail.com>
2021-09-20 15:28:48 -0500

    Began focusing ideas on tracker overhaul

---
+++ WebApplication1/Default.aspx
@@ -0,0 +1,12 @@
+﻿<%@ Page Title="Home Page" Language="C#" MasterPageFile="~/Site.Master" AutoEventWireup="true" CodeBehind="Default.aspx.cs" Inherits="WebApplication1._Default" %>
+
+<asp:Content ID="BodyContent" ContentPlaceHolderID="MainContent" runat="server">
+    <asp:ImageButton id="imagebutton1" runat="server"
+           AlternateText="ImageButton 1"
+           ImageAlign="left"
+           ImageUrl="images/Boss2.png"
+           OnClick="ImageButton_Click"
+           CssClass ="itemNotSelected"/>
+
+
+</asp:Content>
---
+++ WebApplication1/Default.aspx.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace WebApplication1
+{
+    public partial class _Default : Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        { 
+        }
+
+        protected void ImageButton_Click(object sender, EventArgs e)
+        {
+            ImageButton clickedItem = (ImageButton)sender;
+            if (clickedItem.CssClass == "itemNotSelected")
+            {
+                clickedItem.CssClass = "itemSelected";
+            }
+            else
+            {
+                clickedItem.CssClass = "itemNotSelected";
+            }
+            
+        }
+    }
+}
